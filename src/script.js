9f0110cecbb7181f31b56928534fd8291759ed87
Abdelilah Isswa <tomhary66@gmail.com>
2025-11-10 15:28:26 +0100

    add like btn not working

--- src/script.js
+++ src/script.js
@@ -14,11 +14,14 @@ async function getdata(){
 
 let count = 12;
 getdata().then(data=>{
-    
- data.results.forEach((game) => {
+   
+ data.results.forEach((game,index) => {
     //append image
-
+console.log(index)
+if(index<12){
  creatgamecard(game);
+
+}
   
 
  
@@ -30,13 +33,16 @@ getdata().then(data=>{
   button.style.width="50px"
     button.style.backgroundColor="blue"
     document.body.append(button)
-  //////////
+  //////////load more   
 button.addEventListener("click",()=>{
     page++;
     url=`https://debuggers-games-api.duckdns.org/api/games?page=${page}`
      getdata().then(newdata =>{
-        newdata.results.forEach((game) => {
-            creatgamecard(game)
+        newdata.results.forEach((game,index) => {
+            if (index<12) {
+                creatgamecard(game)
+                
+            }
      })
     })
 //     console.log(data.next)
@@ -46,12 +52,28 @@ button.addEventListener("click",()=>{
 })
 
 })
+
+
+///card content
 function creatgamecard(game){
 let body =document.body
+    let like = document.createElement("div")
+   like.textContent = "♥";
+like.style.color = "red";
+like.style.backgroundColor = "white";
+like.style.border = "none";
+like.style.borderRadius = "50%";
+like.style.width = "40px";
+like.style.height = "40px";
+like.style.fontSize = "20px";
+
 
     let container =document.createElement("div")
+    let name = document.createElement("h3")
+    name=game.name
+    console.log(name)
 container.style.backgroundColor="red"
-container.style.height="200px"
+container.style.height="300px"
 container.style.margin="10px"
 
 
@@ -60,7 +82,10 @@ container.style.margin="10px"
 
 let img= document.createElement("img")
 img.src=imgurl;
-/// append name
+
+
  container.append(img)
+ container.append(name)
+ container.append(like)
   body.append(container)
 }
