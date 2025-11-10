848452b5bb7a8f5725d0ae2fa9d91dca084c878b
Abdelilah Isswa <tomhary66@gmail.com>
2025-11-10 12:44:42 +0100

    add image and container

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 
 let url ="https://debuggers-games-api.duckdns.org/api/games"
 
+
 async function getdata(){
     try{console.log("1") 
         const response= await fetch(url);
@@ -10,10 +11,24 @@ async function getdata(){
         console.error(error.message);
     }
 }
+let count = 12;
 getdata().then(data=>{
- console.log("2")
- 
-  console.log(data)
+let body =document.body
+
+    let container =document.createElement("div")
+container.style.backgroundColor="red"
+container.style.height="200px"
+
+
+
+ let imgurl = data.results[0].background_image;
+
+let img= document.createElement("img")
+img.src=imgurl;
+
+
+  body.append(container)
+  container.append(img)
   //////////
 
 
