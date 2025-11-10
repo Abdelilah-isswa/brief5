2d49f486755c01281192e917863757592308230b
Abdelilah Isswa <tomhary66@gmail.com>
2025-11-10 14:36:40 +0100

    add btn to load more games

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 
 let url ="https://debuggers-games-api.duckdns.org/api/games"
 
-
+let page=1;
 async function getdata(){
     try{console.log("1") 
         const response= await fetch(url);
@@ -11,10 +11,42 @@ async function getdata(){
         console.error(error.message);
     }
 }
+
 let count = 12;
 getdata().then(data=>{
+    
  data.results.forEach((game) => {
     //append image
+
+ creatgamecard(game);
+  
+
+ 
+   
+    
+  });
+  let button = document.createElement("button")
+  button.style.height="20px"
+  button.style.width="50px"
+    button.style.backgroundColor="blue"
+    document.body.append(button)
+  //////////
+button.addEventListener("click",()=>{
+    page++;
+    url=`https://debuggers-games-api.duckdns.org/api/games?page=${page}`
+     getdata().then(newdata =>{
+        newdata.results.forEach((game) => {
+            creatgamecard(game)
+     })
+    })
+//     console.log(data.next)
+//    data.page= page
+//     console.log(data.page)
+//     getdata();
+})
+
+})
+function creatgamecard(game){
 let body =document.body
 
     let container =document.createElement("div")
@@ -31,14 +63,4 @@ img.src=imgurl;
 /// append name
  container.append(img)
   body.append(container)
- 
-  
-  console.log(data)
- 
-   
-    
-  });
-  //////////
-
-
-})
+}
