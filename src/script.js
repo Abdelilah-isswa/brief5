bc71668027eada245675bd24d52e4113c8628e98
Abdelilah Isswa <tomhary66@gmail.com>
2025-11-11 10:36:47 +0100

    add popup but not working well img proleme

--- src/script.js
+++ src/script.js
@@ -80,6 +80,7 @@ like.className="border-2  border-blue-500 bg-black-500 rounded h-5 w-1 text-whit
     let container =document.createElement("div")
     let name = document.createElement("h3")
     name=game.name
+
     console.log(name)
 container.style.backgroundColor="red"
 container.style.height="300px"
@@ -92,10 +93,43 @@ container.style.margin="10px"
 let img= document.createElement("img")
 img.src=imgurl;
 img.className =" "
-
+console.log("1")
+console.log(game.name)
 
  container.append(img)
  container.append(name)
  container.append(like)
   body.append(container)
+
+  //////voir les dtails dun jeux on click
+container.addEventListener("click", () => {
+    // Create blur overlay
+    const overlay = document.createElement("div");
+    overlay.className = "fixed inset-0 backdrop-blur-md bg-black bg-opacity-30 z-40";
+    document.body.appendChild(overlay);
+    
+    // Create popup
+    let popup = document.createElement("div");
+    popup.className = "bg-white p-6 rounded-lg shadow-xl h-1/2 w-1/2 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 overflow-auto";
+    document.body.appendChild(popup);
+    
+    popup.append(img);
+    popup.append(name);
+    popup.append(game.description);
+    
+    // Close functionality
+    function closeModal() {
+        document.body.removeChild(overlay);
+        document.body.removeChild(popup);
+        document.removeEventListener("keydown", handleEscape);
+    }
+    
+    overlay.addEventListener("click", closeModal);
+    
+    // Close with ESC key
+    function handleEscape(e) {
+        if (e.key === 'Escape') closeModal();
+    }
+    document.addEventListener("keydown", handleEscape);
+});
 }
